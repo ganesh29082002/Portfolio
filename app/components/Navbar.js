fad02af8798bd6262bb2e2f9678d6ea769ad8530
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2024-02-16 17:19:07 +0530

    navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div>
+
       <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 ">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between md:text-start mx-auto p-4">
           <a
@@ -113,6 +113,6 @@ export default function Navbar() {
         </div>
         
       </nav>
-    </div>
+   
   );
 }
