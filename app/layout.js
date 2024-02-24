7c9f257d481ddd40160e7d09a0c7b39ac312d26d
ganesh29082002 <83921516+ganesh29082002@users.noreply.github.com>
2024-02-24 10:09:28 +0530

    async added in script

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
        <head>
-       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" rel="stylesheet"/>
+       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" rel="stylesheet" async/>
        </head>
       <body className={inter.className}>
         
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js" async />
         <Navbar/>
         {children}
         <Footer/>
